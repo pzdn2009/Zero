d69ad9e40ebfe4a67bf70f5d2cf05e9cfa2bef4a
pzdn2009 <1050244110@qq.com>
2016-09-10 23:10:49 +0800

    add the IEntity interface

---
+++ src/Zero.Domain/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Zero.Domain
+{
+    /// <summary>
+    /// The default IEntity interface with the type of long.
+    /// </summary>
+    public interface IEntity : IEntity<long>
+    {
+
+    }
+}
---
+++ src/Zero.Domain/IEntity`.cs
@@ -0,0 +1,14 @@
+﻿namespace Zero.Domain
+{
+    /// <summary>
+    /// Define an interface representes the DDD Entity, has a property named Id, which is generic type.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the Id</typeparam>
+    public interface IEntity<TKey>
+    {
+        /// <summary>
+        /// Identity the entity object.
+        /// </summary>
+        TKey Id { get; set; }
+    }
+}
