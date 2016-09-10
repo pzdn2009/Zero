62765d0c98d62afa00aa2ac6bc9734485bc70d9f
pzdn2009 <1050244110@qq.com>
2016-09-10 21:07:59 +0800

    #issue1 create the solution.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zero
+A domain-driven-development framework focused on the asp .net core .
+
+Status : In developing... ,
+
+Add us : 559617625(qq groups)
---
+++ test/Zero.Application.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zero.Application.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee019a2f-112d-4bca-97ea-aef7fb2b7234")]
